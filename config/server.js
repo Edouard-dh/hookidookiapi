ffe00eafc88af85762a933dd344f2f281c817732
Edouard Dhalluin <ed.dhalluin@gmail.com>
2021-09-07 14:16:13 +0200

    création de l'api sous strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1eeb902cc5106e10f7042d759441c75'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a3b2cd0-d8b6-454d-b498-67294ab18ba3'
+};
